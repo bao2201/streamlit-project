4449259472c44d6c20583f93c74a6532d491f4bb
bao2201 <88237459+bao2201@users.noreply.github.com>
2021-10-20 22:26:53 +0700

    Add files via upload

---
+++ heart_python.py
@@ -0,0 +1,33 @@
+import turtle
+pen = turtle.Turtle()
+pen.speed(0)
+pen.hideturtle()
+def curve():
+    for i in range(200):
+        pen.right(1)
+        pen.forward(1)
+def heart():
+    pen.fillcolor('red')
+    pen.begin_fill()
+    pen.left(140)
+    pen.forward(113)
+    curve()
+    pen.left(120)
+    curve()
+    pen.forward(112)
+    pen.end_fill()
+def txt():
+    pen.up()
+  
+    pen.setpos(-70, 100)
+
+    pen.down()
+
+    pen.color('lightpink')
+
+    pen.write("Chúc mừng 20/10", font=(
+      "Verdana", 12, "bold"))
+heart()
+txt()
+turtle.done()
+
